927ee4692776a22533853e22edcf43fddf550fcc
amandalynne <amandalynne.p@gmail.com>
2016-12-11 18:04:46 -0800

    finalized training data structure (dict)

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-
+import re
 import sys
 
 from preproc import *
@@ -14,6 +14,8 @@ def main():
 		training = train_step(sys.argv[1], sys.argv[2])
 
 
+
+
 def process_summaries(input_dir):
     """Create training data structure from summary.out files"""
     ret = {}
@@ -22,17 +24,32 @@ def process_summaries(input_dir):
             ret[subdirname] = {}
             summary_file = os.path.join(input_dir,subdirname,'4-output', 'summary.out')
             if os.path.isfile(summary_file):
-                print(summary_file)
-                    #ret[subdirname][doc] = filename_to_doc(os.path.join(dirname,subdirname,'3-txt',filename))
+                with open(summary_file) as inf:
+                    for line in inf.readlines():
+                        pattern = r'^\s*\%+\s*(\w+\d+)\s+(\d+)/(\d+)\s+([^#]+)\s+##([^%]+)\s+%%(.*)$'
+                        match = re.match(pattern, line)
+                        if match:
+                            document = match.group(1).rstrip()
+                            attraction = match.group(4).rstrip()
+                            category = match.group(5).rstrip()
+                            description = match.group(6).rstrip()
+                            # need to take off leading whitespace
+                            ret[subdirname][attraction] = {}
+                            ret[subdirname][attraction]['desc'] = {}
+                            ret[subdirname][attraction]['desc'][document] =\
+                                (description, category)
+        print(ret['argentina-annot1'])
+        return ret
+                            
 
 def train_step(input_dir, model_dir):
 
-	#####	STEP 1 : preproc.py			#####
-	#docs = dirname_to_docs('../annot2/3-txt')
-	#docs = dirname_to_docs(input_dir)
+    #####	STEP 1 : preproc.py			#####
+    #docs = dirname_to_docs('../annot2/3-txt')
+    #docs = dirname_to_docs(input_dir)
 
     process_summaries(input_dir)
-    #####	STEP 2 : ner.py				#####
+    ####
     entities = docs_to_named_entities(docs)
 
     #####	STEP 3 : attractions.py		#####
