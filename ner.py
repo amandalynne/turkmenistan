032b1e09f432145e21c346d049641ef2d7740cd4
amandalynne <amandalynne.p@gmail.com>
2016-12-13 14:13:25 -0800

    some refinement of NER and getting closer to having that sweet sweet data structure built

--- ner.py
+++ ner.py
@@ -1,7 +1,5 @@
 import nltk
-
-# look for rank
-# look for word shape
+import re
 
 """
 { 'annot1' : { 'Statue': {'d2' : {'rank' : '9/10', 'desc' : [], 'category': '' } } } }
@@ -11,21 +9,24 @@ import nltk
 ####################
 
 def docs_to_named_entities(docs):
-        grammar = "RANKED: {<CD><DT>*<NNP><NN.*>*<PP>*<NN.*>*}"
+        grammar = "RANKED: {<CD><DT>*<JJ>*<NNP><NN.*>*<PP>*<NN.*>*}"
         chunker = nltk.RegexpParser(grammar)
+        digit_pattern = r'^\d+$'
         ret = {}
-        attractions = [] 
         for annot in docs:
+                annot_attractions = []
                 ret[annot] = {}
                 for doc in docs[annot]:
-                        attractions = []
+                        doc_attractions = []
                         for i, line in enumerate(docs[annot][doc]):
                                 for j, sent in enumerate(line):
                                         tree = chunker.parse(sent['pos'])
                                         for subtree in tree.subtrees():
                                                 if subtree.label() == 'RANKED':
-                                                        print(doc, subtree)
-                                                        attractions.append(subtree)
+                                                        chunk = [x[0] for x in subtree.leaves()]
+                                                        if re.match(digit_pattern, chunk[0]):
+                                                                doc_attractions.append(chunk)
+                        print(annot, doc, doc_attractions)
                 # ret[annot][entity] = {}
                 # ret[annot][entity][doc] = {}
                 # ret[annot][entity][doc]['rank'] = N 
@@ -33,6 +34,7 @@ def docs_to_named_entities(docs):
                 # ret[annot][entity][doc]['category'] = N 
                 # ret[annot][entity][doc]['instances'] = []
         return ret
+
 
 ####################
 
